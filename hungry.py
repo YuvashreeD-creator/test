853bf19d48a28db71f4b26eb01423a3a8581da47
YuvashreeD19 <yuvashreedayalan19@gmail.com>
2025-10-31 15:31:23 +0530

    thirsty

--- hungry.py
+++ hungry.py
@@ -4,4 +4,6 @@ if hungry=="yes":
     print("Eat Chicken")
     print("Revert1")
 else:
-    print("Do your work.")
+    thirsty=input("Are you thirsty?")
+    if thirsty=="yes":
+        print("Drink water")
